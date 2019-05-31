c78c0f9132bab4f6f989c3def62974fe91c30a89
AriEtemadi <arianetemadi35@gmail.com>
2019-05-31 12:36:39 +0430

    Designing...

--- src/ChatsMenu.java
+++ src/ChatsMenu.java
@@ -1,4 +1,6 @@
 import javafx.animation.AnimationTimer;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,26 +17,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 class ChatsMenu {
-    private static final int MAX_MSG_NUM = 20;
     private Client client;
     private Chat chat;
+
+    private final int MAX_MSG_NUM = 20;
     private final int WIDTH = 350;
     private final int HEIGHT = 400;
     private final int BORDER_DIST = 5;
+    private final String BACKGROUND_COLOR = "-fx-background-color: #18191D;";
+    private final String CHATS_COLOR = "-fx-background-color: #282E33;";
+    private final String SELECTED_CHAT_COLOR = "-fx-background-color: #009687;";
+    private final String RIGHT_CLICKED_COLOR = "-fx-background-color: #65AADD;";
+    private final String TEXT_COLOR = "FFFFFF";
+    private final String TEXTFIELD_COLOR = "-fx-background-color: #3D444B; -fx-text-fill: #ffffff";
+
+    private Scene scene;
     private Group root;
+    private Group chatGroup;
+    private Group messageGroup;
+
     private VBox chatsColumn;
-    private Group msgGroup;
+
     private TextField input;
-    private TextField groupName;
-    private Label createGroupErrMsg;
-    private Button addMember;
+    private HBox emojis;
     private Button send;
     private Button back;
-    private Button selectedChat;
-    private Button createGroup;
-    private HBox emojis;
     private VBox messages;
-    private Scene scene;
+
+    private TextField groupName;
+    private Button addMember;
+    private Label createGroupErrMsg;
+    private Button createGroup;
+
+    private Button selectedChat;
     private List<Button> rightClickedButtons = new ArrayList<>();
 
     ChatsMenu(Client client) {
@@ -43,36 +58,91 @@ class ChatsMenu {
 
     void run() {
         root = new Group();
-        scene = new Scene(root, WIDTH, HEIGHT, Color.valueOf("4a6084"));
+        scene = new Scene(root, WIDTH, HEIGHT, Color.valueOf("18191D"));
         View.getInstance().setScene(scene);
 
+        initChatGroup();
+        initMessageGroup();
+        initCreateGroupNodes();
+
+        root.getChildren().addAll(chatGroup, createGroup, groupName, createGroupErrMsg);
+    }
+
+    private void initChatGroup() {
+        chatGroup = new Group();
+
         initChatsColumn();
-        initAddMember();
-        initMsgGroup();
-        initCreateGroup();
-        initGroupName();
-        initCreateGroupErrMsg();
 
         showChatsColumn();
-        showAddMember();
 
-        root.getChildren().addAll(chatsColumn, createGroup, groupName, createGroupErrMsg);
-
-        createGroupButtonAction();
-        groupNameButtonAction();
-        addMemberAction();
+        chatGroup.getChildren().add(chatsColumn);
     }
 
     private void initChatsColumn() {
         chatsColumn = new VBox();
-        chatsColumn.relocate(BORDER_DIST, BORDER_DIST);
+        chatsColumn.relocate(0, 0);
         chatsColumn.setPrefWidth(100);
+        chatsColumn.setPrefHeight(500);
         chatsColumn.setSpacing(3);
+        chatsColumn.setStyle(CHATS_COLOR);
+        Button chats = new Button("CHATS");
+        chats.setTextFill(Color.valueOf(TEXT_COLOR));
+        chatsColumn.getChildren().add(chats);
+        chats.setPrefWidth(ChatsMenu.this.chatsColumn.getPrefWidth());
+        chats.setStyle(SELECTED_CHAT_COLOR);
+        chats.setShape(new Rectangle(1, 1));
     }
 
-    private void initMsgGroup() {
-        msgGroup = new Group();
-        msgGroup.relocate(BORDER_DIST * 2 + chatsColumn.getPrefWidth() + 20, BORDER_DIST);
+    private void showChatsColumn() {
+        AnimationTimer chatsColumn = new AnimationTimer() {
+            long last = 0;
+
+            @Override
+            public void handle(long now) {
+                if (client.getUser() != null)
+                    if (now - last > 100) {
+                        List<Chat> chats = client.getUser().getChats();
+                        for (Chat chat : chats) {
+                            if (!client.isShowed(chat)) {
+                                client.addShowedChat(chat);
+                                Button button = new Button();
+                                button.setText(chat.getNameFor(client.getUser()));
+                                button.setTextFill(Color.valueOf(TEXT_COLOR));
+                                button.setOnAction(event -> {
+                                    if (selectedChat != null)
+                                        selectedChat.setStyle(CHATS_COLOR);
+                                    ChatsMenu.this.chat = chat;
+                                    button.setStyle(SELECTED_CHAT_COLOR);
+                                    selectedChat = button;
+                                });
+                                button.setOnMouseReleased(event -> {
+                                    if (event.getButton() == MouseButton.SECONDARY)
+                                        if (rightClickedButtons.contains(button)) {
+                                            rightClickedButtons.remove(button);
+                                            if (selectedChat == button)
+                                                button.setStyle(SELECTED_CHAT_COLOR);
+                                            else
+                                                button.setStyle(CHATS_COLOR);
+                                        } else {
+                                            rightClickedButtons.add(button);
+                                            button.setStyle(RIGHT_CLICKED_COLOR);
+                                        }
+                                });
+                                button.setPrefWidth(ChatsMenu.this.chatsColumn.getPrefWidth());
+                                button.setStyle(CHATS_COLOR);
+                                button.setShape(new Rectangle(1, 1));
+                                ChatsMenu.this.chatsColumn.getChildren().add(button);
+                            }
+                        }
+                    }
+            }
+        };
+        chatsColumn.start();
+    }
+
+    private void initMessageGroup() {
+        messageGroup = new Group();
+        messageGroup.relocate(BORDER_DIST * 2 + chatsColumn.getPrefWidth() + 20, BORDER_DIST);
 
         initEmojis();
         initMessages();
@@ -82,10 +152,35 @@ class ChatsMenu {
 
         showMessages();
 
-        msgGroup.getChildren().addAll(messages, input, send, back, emojis);
+        messageGroup.getChildren().addAll(messages, input, send, back, emojis);
 
         backAction();
         sendButtonAction();
+    }
+
+    private void initMessages() {
+        messages = new VBox();
+        messages.relocate(0, 0);
+    }
+
+    private void initInput() {
+        input = new TextField();
+        input.setPromptText("MESSAGE");
+        input.relocate(0, HEIGHT - 35);
+        input.setPrefWidth(165);
+        input.setFocusTraversable(false);
+        input.setOnAction(event -> sendAction());
+        input.setStyle(TEXTFIELD_COLOR);
+    }
+
+    private void initSend() {
+        send = new Button("SEND");
+        send.relocate(170, HEIGHT - 35);
+    }
+
+    private void initBack() {
+        back = new Button("BACK");
+        back.relocate(170, HEIGHT - 35 * 2);
     }
 
     private void initEmojis() {
@@ -99,55 +194,6 @@ class ChatsMenu {
                 input.setText(input.getText().concat(s));
             });
         }
-    }
-
-    private void initMessages() {
-        messages = new VBox();
-        messages.relocate(0, 0);
-    }
-
-    private void showMessages() {
-        AnimationTimer animationTimer = new AnimationTimer() {
-            long last = 0;
-
-            @Override
-            public void handle(long now) {
-                if (now - last > 100) {
-                    messages.getChildren().remove(0, messages.getChildren().size());
-                    if (chat != null) {
-                        if (!root.getChildren().contains(msgGroup))
-                            root.getChildren().add(msgGroup);
-                        for (int i = Math.max(chat.getMessages().size() - MAX_MSG_NUM, 0); i < chat.getMessages().size(); i++) {
-                            String message = chat.getMessages().get(i);
-                            Label msg = new Label(message);
-                            messages.getChildren().add(msg);
-                        }
-                        messages.relocate(0, 350 - messages.getHeight());
-                    }
-                    last = now;
-                }
-            }
-        };
-        animationTimer.start();
-    }
-
-    private void initInput() {
-        input = new TextField();
-        input.setPromptText("MESSAGE");
-        input.relocate(0, HEIGHT - 35);
-        input.setPrefWidth(165);
-        input.setFocusTraversable(false);
-        input.setOnAction(event -> sendAction());
-    }
-
-    private void initSend() {
-        send = new Button("SEND");
-        send.relocate(170, HEIGHT - 35);
-    }
-
-    private void initBack() {
-        back = new Button("BACK");
-        back.relocate(170, HEIGHT - 35 * 2);
     }
 
     private void backAction() {
@@ -165,55 +211,96 @@ class ChatsMenu {
         input.setText("");
     }
 
-    private void showChatsColumn() {
+    private void showMessages() {
+        AnimationTimer showMessages = new AnimationTimer() {
+            long last = 0;
+
+            @Override
+            public void handle(long now) {
+                if (now - last > 100) {
+                    messages.getChildren().remove(0, messages.getChildren().size());
+                    if (chat != null) {
+                        if (!root.getChildren().contains(messageGroup))
+                            root.getChildren().add(messageGroup);
+                        for (int i = Math.max(chat.getMessages().size() - MAX_MSG_NUM, 0); i < chat.getMessages().size(); i++) {
+                            String message = chat.getMessages().get(i);
+                            Label msg = new Label(message);
+                            msg.setTextFill(Color.valueOf(TEXT_COLOR));
+                            messages.getChildren().add(msg);
+                        }
+                        messages.relocate(0, 350 - messages.getHeight());
+                    }
+                    last = now;
+                }
+            }
+        };
+        showMessages.start();
+    }
+
+    private void initCreateGroupNodes() {
+        initCreateGroup();
+        initAddMember();
+        initGroupName();
+        initCreateGroupErrMsg();
+
+        showAddMember();
+
+        createGroupButtonAction();
+        groupNameButtonAction();
+        addMemberAction();
+    }
+
+    private void initCreateGroup() {
+        createGroup = new Button("CREATE GROUP");
+        createGroup.relocate(0, HEIGHT - 53);
+        createGroup.setPrefWidth(ChatsMenu.this.chatsColumn.getPrefWidth());
+        createGroup.setStyle("-fx-background-color: #df5b5e;");
+        createGroup.setShape(new Rectangle(1, 1));
+        createGroup.setTextFill(Color.valueOf(TEXT_COLOR));
+        createGroup.setFont(Font.font(11));
+    }
+
+    private void initCreateGroupErrMsg() {
+        createGroupErrMsg = new Label("");
+        createGroupErrMsg.relocate(BORDER_DIST, HEIGHT - 77);
+        createGroupErrMsg.setTextFill(Color.ORANGE);
+        createGroupErrMsg.setFont(Font.font(9));
+    }
+
+    private void initAddMember() {
+        addMember = new Button("ADD MEMBER");
+        addMember.relocate(BORDER_DIST, HEIGHT - 105);
+    }
+
+    private void initGroupName() {
+        groupName = new TextField();
+        groupName.setPromptText("GROUP NAME");
+        groupName.setPrefWidth(chatsColumn.getPrefWidth());
+        groupName.relocate(0, HEIGHT - 25);
+        groupName.setFocusTraversable(false);
+        groupName.setFont(Font.font(10));
+        groupName.setStyle(TEXTFIELD_COLOR);
+        groupName.setAlignment(Pos.CENTER);
+    }
+
+    private void showAddMember() {
         AnimationTimer animationTimer = new AnimationTimer() {
             long last = 0;
 
             @Override
             public void handle(long now) {
-                if (client.getUser() != null)
-                    if (now - last > 100) {
-                        List<Chat> chats = client.getUser().getChats();
-                        for (Chat chat : chats) {
-                            if (!client.isShowed(chat)) {
-                                client.addShowedChat(chat);
-                                Button button = new Button();
-                                button.setText(chat.getNameFor(client.getUser()));
-                                button.setOnAction(event -> {
-                                    if (selectedChat != null)
-                                        selectedChat.setStyle("-fx-background-color: #ffffff;");
-                                    ChatsMenu.this.chat = chat;
-                                    button.setStyle("-fx-background-color: #ffff00;");
-                                    selectedChat = button;
-                                });
-                                button.setOnMouseReleased(event -> {
-                                    if (event.getButton() == MouseButton.SECONDARY)
-                                        if (rightClickedButtons.contains(button)) {
-                                            rightClickedButtons.remove(button);
-                                            if (selectedChat == button)
-                                                button.setStyle("-fx-background-color: #ffff00;");
-                                            else
-                                                button.setStyle("-fx-background-color: #ffffff;");
-                                        } else {
-                                            rightClickedButtons.add(button);
-                                            button.setStyle("-fx-background-color: #ff00ff;");
-                                        }
-                                });
-                                button.setPrefWidth(chatsColumn.getPrefWidth());
-                                button.setStyle("-fx-background-color: #ffffff;");
-                                button.setShape(new Rectangle(1, 1));
-                                chatsColumn.getChildren().add(button);
-                            }
+                if (now - last > 100) {
+                    if (selectedChat != null
+                            && Chat.isThisTheMakerOf(client.getUser(), selectedChat.getText())) {
+                        if (!root.getChildren().contains(addMember)) {
+                            root.getChildren().add(addMember);
                         }
-                    }
+                    } else root.getChildren().remove(addMember);
+                    last = now;
+                }
             }
         };
         animationTimer.start();
-    }
-
-    private void initCreateGroup() {
-        createGroup = new Button("CREATE GROUP");
-        createGroup.relocate(BORDER_DIST, HEIGHT - 30);
     }
 
     private void createGroupButtonAction() {
@@ -221,14 +308,6 @@ class ChatsMenu {
             createGroupAction();
             eraseRightClicked();
         });
-    }
-
-    private void initGroupName() {
-        groupName = new TextField();
-        groupName.setPromptText("GROUP NAME");
-        groupName.setPrefWidth(chatsColumn.getPrefWidth());
-        groupName.relocate(BORDER_DIST, HEIGHT - 62);
-        groupName.setFocusTraversable(false);
     }
 
     private void groupNameButtonAction() {
@@ -241,9 +320,9 @@ class ChatsMenu {
     private void eraseRightClicked() {
         for (Button button : rightClickedButtons)
             if (selectedChat == button)
-                button.setStyle("-fx-background-color: #ffff00;");
+                button.setStyle(SELECTED_CHAT_COLOR);
             else
-                button.setStyle("-fx-background-color: #ffffff;");
+                button.setStyle(CHATS_COLOR);
         rightClickedButtons = new ArrayList<>();
     }
 
@@ -267,18 +346,6 @@ class ChatsMenu {
         groupName.setText("");
     }
 
-    private void initCreateGroupErrMsg() {
-        createGroupErrMsg = new Label("");
-        createGroupErrMsg.relocate(BORDER_DIST, HEIGHT - 77);
-        createGroupErrMsg.setTextFill(Color.RED);
-        createGroupErrMsg.setFont(Font.font(9));
-    }
-
-    private void initAddMember() {
-        addMember = new Button("ADD MEMBER");
-        addMember.relocate(BORDER_DIST, HEIGHT - 105);
-    }
-
     private void addMemberAction() {
         addMember.setOnAction(event -> {
             if (rightClickedButtons.size() < 1)
@@ -295,25 +362,5 @@ class ChatsMenu {
                 createGroupErrMsg.setText("ERROR");
             eraseRightClicked();
         });
-    }
-
-    private void showAddMember() {
-        AnimationTimer animationTimer = new AnimationTimer() {
-            long last = 0;
-
-            @Override
-            public void handle(long now) {
-                if (now - last > 100) {
-                    if (selectedChat != null
-                            && Chat.isThisTheMakerOf(client.getUser(), selectedChat.getText())) {
-                        if (!root.getChildren().contains(addMember)) {
-                            root.getChildren().add(addMember);
-                        }
-                    } else root.getChildren().remove(addMember);
-                    last = now;
-                }
-            }
-        };
-        animationTimer.start();
     }
 }
